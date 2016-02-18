3c609095bd7745d128a0f775f08b78f8ef4b558d
Matthew Furseman <matthew.furseman@diamond.ac.uk>
2016-02-18 09:01:35 +0000

    Move imports to nebula

--- plugins/org.csstudio.opibuilder.widgets.dawn/src/org/csstudio/opibuilder/widgets/dawn/intensitygraph/DawnIntensityGraphModel.java
+++ plugins/org.csstudio.opibuilder.widgets.dawn/src/org/csstudio/opibuilder/widgets/dawn/intensitygraph/DawnIntensityGraphModel.java
@@ -22,11 +22,11 @@ import org.csstudio.opibuilder.properties.WidgetPropertyCategory;
 import org.csstudio.opibuilder.util.MediaService;
 import org.csstudio.opibuilder.util.OPIColor;
 import org.csstudio.opibuilder.util.UpgradeUtil;
-import org.csstudio.opibuilder.widgets.util.SingleSourceHelper;
-import org.csstudio.swt.widgets.datadefinition.ColorMap;
-import org.csstudio.swt.widgets.datadefinition.ColorMap.PredefinedColorMap;
-import org.csstudio.swt.widgets.figures.IntensityGraphFigure.ColorDepth;
 import org.csstudio.ui.util.CustomMediaFactory;
+import org.eclipse.nebula.visualization.widgets.datadefinition.ColorMap;
+import org.eclipse.nebula.visualization.widgets.datadefinition.ColorMap.PredefinedColorMap;
+import org.eclipse.nebula.visualization.widgets.figures.IntensityGraphFigure.ColorDepth;
+import org.eclipse.nebula.visualization.xygraph.util.SingleSourceHelper;
 import org.eclipse.swt.graphics.RGB;
 import org.osgi.framework.Version;
 
