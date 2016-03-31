623e3e2c11b2d36096b434040413b1c80328dd84
Will Rogers <wahrogers@gmail.com>
2016-03-31 13:24:15 +0100

    Restructure to meet new requirements.
    * If starting CS-Studio, open the first window in the specified perspective
    * Otherwise, open a new window in the specified perspective.

--- plugins/org.csstudio.openfile.newwindow/src/org/csstudio/openfile/newwindow/OpenNewWindow.java
+++ plugins/org.csstudio.openfile.newwindow/src/org/csstudio/openfile/newwindow/OpenNewWindow.java
@@ -2,6 +2,8 @@ package org.csstudio.openfile.newwindow;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,6 +13,8 @@ import java.util.logging.Level;
 import org.csstudio.openfile.IOpenDisplayAction;
 
 public class OpenNewWindow implements IOpenDisplayAction {
+
+    private static final int STARTUP_TIME_MILLIS = 15000;
 
     @Override
     public void openDisplay(String path, String data) {
@@ -24,9 +28,16 @@ public class OpenNewWindow implements IOpenDisplayAction {
                     updateLinks(windowSpec.getLinks());
                 }
                 PerspectiveHelper perspectiveHelper = new PerspectiveHelper(windowSpec);
-                perspectiveHelper.checkAndLoad();
+                perspectiveHelper.loadPerspective(windowSpec.getPerspectiveFile());
                 WindowOpener windowOpener = new WindowOpener(windowSpec);
-                windowOpener.raiseOrOpenWindow();
+                // If CS-Studio is opening for the first time, the first window should open
+                // in the specified perspective.  Otherwise, a new window should open in the
+                // specified perspective.
+                if (newlyStarted()) {
+                    windowOpener.changePerspective();
+                } else {
+                    windowOpener.openWindow();
+                }
             } catch (IOException | WindowManagementException e) {
                 Plugin.getLogger().log(Level.WARNING, "Failed to load window spec file", e);
             }
@@ -40,6 +51,19 @@ public class OpenNewWindow implements IOpenDisplayAction {
         } catch (InterruptedException e) {
             Plugin.getLogger().log(Level.WARNING, "Failed to update links", e);
         }
+    }
+
+    /**
+     * Return whether this is a new instance of CS-Studio.
+     *
+     * This is a hack.  It checks if the JVM is more than STARTUP_TIME_MILLIS old.
+     * If (for example) CS-Studio prompts for a workspace, this check could easily
+     * return the wrong answer.
+     * @return whether CS-Studio has just started
+     */
+    private boolean newlyStarted() {
+        RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
+        return runtimeBean.getUptime() < STARTUP_TIME_MILLIS;
     }
 
 }
--- plugins/org.csstudio.openfile.newwindow/src/org/csstudio/openfile/newwindow/WindowOpener.java
+++ plugins/org.csstudio.openfile.newwindow/src/org/csstudio/openfile/newwindow/WindowOpener.java
@@ -2,7 +2,7 @@ package org.csstudio.openfile.newwindow;
 
 import java.util.logging.Level;
 
-import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.WorkbenchException;
@@ -15,67 +15,20 @@ public class WindowOpener {
         this.windowSpec = windowSpec;
     }
 
-    public void raiseOrOpenWindow() throws WindowManagementException {
-        boolean found = false;
-        for (IWorkbenchWindow w : PlatformUI.getWorkbench().getWorkbenchWindows()) {
-            String windowId = w.getActivePage().getPerspective().getId();
-            if (windowId.equals(windowSpec.getPerspectiveId())) {
-                System.out.println("Found!");
-                forceActive(w.getShell());
-                found = true;
-            } else {
-                System.out.println("Other perspective " + windowId);
-            }
-        }
-        if (!found) {
-            try {
-                PlatformUI.getWorkbench().openWorkbenchWindow(windowSpec.getPerspectiveId(), null);
-            } catch (WorkbenchException e) {
-                Plugin.getLogger().log(Level.WARNING, "Failed to open new workbench window", e);
-            }
+    public void openWindow() throws WindowManagementException {
+        try {
+            PlatformUI.getWorkbench().openWorkbenchWindow(windowSpec.getPerspectiveId(), null);
+        } catch (WorkbenchException e) {
+            Plugin.getLogger().log(Level.WARNING, "Failed to open new workbench window", e);
         }
     }
 
-    public void forceActive(Shell shell) {
-        shell.getDisplay().asyncExec(new Runnable() {
-
-            @Override
-            public void run() {
-                //if (!shell.getMaximized()) {
-  //              shell.setVisible(false);
-                    shell.setMaximized(true);
-                //} else if (shell.getMinimized()) {
-                    shell.setMinimized(false);
-                    shell.setMaximized(false);
-//                shell.forceFocus();
-//                shell.forceActive();
-//                shell.setFocus();
-//                shell.setActive();
-             //  }
-                shell.forceActive();
-            }
-        });
-        shell.getDisplay().asyncExec(new Runnable() {
-
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    //
-                }
-
-//                shell.setActive();
-//                shell.forceActive();
-//
-//                //shell.setEnabled(false);
-//                shell.setVisible(true);
-                //shell.setEnabled(true);
-                shell.open();
-            }
-        });
-
-      }
-
+    public void changePerspective() {
+        IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+        IPerspectiveDescriptor descriptor = window.getWorkbench()
+                .getPerspectiveRegistry()
+                .findPerspectiveWithId(windowSpec.getPerspectiveId());
+        window.getActivePage().setPerspective(descriptor);
+    }
 
 }
