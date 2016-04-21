21730e66a48cf2cca09c6dded762002d8a3d4da8
Will Rogers <wahrogers@gmail.com>
2016-04-21 16:23:02 +0100

    Add a custom DataProvider for Dawn XYGraph.
    This returns the X index value if the dataprovider is aware that:
    
    * the data is chronological
    * the X axis isn't plotting dates
    
    This avoids infinitely increasing integers on the X axis.

---
+++ plugins/org.csstudio.opibuilder.widgets.dawn/src/org/csstudio/opibuilder/widgets/dawn/xygraph/XygraphDataProvider.java
@@ -0,0 +1,55 @@
+package org.csstudio.opibuilder.widgets.dawn.xygraph;
+
+import org.eclipse.nebula.visualization.xygraph.dataprovider.CircularBufferDataProvider;
+import org.eclipse.nebula.visualization.xygraph.dataprovider.IDataProvider;
+import org.eclipse.nebula.visualization.xygraph.dataprovider.ISample;
+import org.eclipse.nebula.visualization.xygraph.dataprovider.Sample;
+import org.eclipse.nebula.visualization.xygraph.linearscale.Range;
+
+public class XygraphDataProvider extends CircularBufferDataProvider
+        implements IDataProvider {
+
+    private boolean xAxisUsingDate;
+
+    public XygraphDataProvider(boolean chronological, boolean xAxisUsingDate) {
+        super(chronological);
+        this.xAxisUsingDate = xAxisUsingDate;
+    }
+
+    public void setXAxisUsingDate(boolean xAxisUsingDate) {
+        this.xAxisUsingDate = xAxisUsingDate;
+    }
+
+    public boolean plotXIndex() {
+        return chronological && !xAxisUsingDate;
+    }
+
+    @Override
+    public ISample getSample(int index) {
+        ISample sample = super.getSample(index);
+        if (sample != null && plotXIndex()) {
+            sample = new Sample(index, sample.getYValue(),
+                                sample.getYPlusError(), sample.getYMinusError(),
+                                0, 0,
+                                sample.getInfo());
+        }
+        return sample;
+    }
+
+    @Override
+    public synchronized Range getXDataMinMax() {
+        Range xRange;
+        if (plotXIndex() && getSize() > 1) {
+            xRange = new Range(0, traceData.size() - 1);
+        } else {
+            xRange = super.getXDataMinMax();
+        }
+        return xRange;
+    }
+
+    @Override
+    public void setChronological(boolean chronological) {
+        super.setChronological(chronological);
+    }
+
+}
