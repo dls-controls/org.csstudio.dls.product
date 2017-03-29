a40ce0a1e60e238d8f298b34f3db671886903745
Will Rogers <wahrogers@gmail.com>
2017-03-29 09:47:48 +0100

    Fix test for changed new window behaviour.

--- plugins/org.csstudio.openfile.newwindow.test/src/org/csstudio/openfile/newwindow/WindowSpecParserTest.java
+++ plugins/org.csstudio.openfile.newwindow.test/src/org/csstudio/openfile/newwindow/WindowSpecParserTest.java
@@ -96,15 +96,6 @@ public class WindowSpecParserTest {
     }
 
     @Test(expected=WindowManagementException.class)
-    public void xmlWithNoFileThrowsException() throws WindowManagementException {
-        List<String> oneId = createStringList(helloId);
-        String noFileXml = createXml(oneId, noFiles, noLinks);
-        WindowSpecParser parser = createParser(noFileXml);
-
-        parser.parse();
-    }
-
-    @Test(expected=WindowManagementException.class)
     public void xmlWithTwoPerspectiveIdsThrowsException() throws WindowManagementException {
         List<String> twoIds = createStringList(helloId, byeId);
         List<String> oneFile = createStringList(helloFile);
@@ -124,6 +115,15 @@ public class WindowSpecParserTest {
         parser.parse();
     }
 
+    public void xmlWithNoFileCreatesWindowSpec() throws WindowManagementException {
+        List<String> oneId = createStringList(helloId);
+        String noFileXml = createXml(oneId, noFiles, noLinks);
+        WindowSpecParser parser = createParser(noFileXml);
+
+        WindowSpec spec = parser.parse();
+        assertEquals(helloId, spec.getPerspectiveId());
+    }
+
     @Test
     public void simpleXmlFetchesPerspectiveIdAndFile() throws WindowManagementException {
         List<String> ids = createStringList(helloId);
@@ -133,7 +133,7 @@ public class WindowSpecParserTest {
 
         WindowSpec spec = simpleParser.parse();
 
-        assertEquals(helloId, spec.getPerspectiveId());
+
         assertEquals(helloFile, spec.getPerspectiveFile());
     }
 
