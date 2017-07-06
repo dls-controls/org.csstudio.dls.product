28f934ce2f67d9957c3d31dd6636c31676776f1f
Will Rogers <wahrogers@gmail.com>
2017-07-06 12:30:52 +0100

    Separate TrayIcon class.  Add TrayListener.

--- plugins/org.csstudio.product.trayicon/src/org/csstudio/trayicon/TrayApplicationWorkbenchAdvisor.java
+++ plugins/org.csstudio.product.trayicon/src/org/csstudio/trayicon/TrayApplicationWorkbenchAdvisor.java
@@ -1,102 +1,34 @@
 package org.csstudio.trayicon;
 
 import org.csstudio.startup.application.OpenDocumentEventProcessor;
-import org.csstudio.utility.product.Activator;
 import org.csstudio.utility.product.ApplicationWorkbenchAdvisor;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.MenuItem;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.TrayItem;
-import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.ui.PlatformUI;
 
 public class TrayApplicationWorkbenchAdvisor extends ApplicationWorkbenchAdvisor {
 	
-    private boolean minimizedToTray = false;
+    private TrayIcon trayIcon;
 	
     public TrayApplicationWorkbenchAdvisor(OpenDocumentEventProcessor openDocProcessor) {
-    	super(openDocProcessor);
+        super(openDocProcessor);
+        trayIcon = new TrayIcon();
     }
 
-    private void minimizeToTray() {
-        final TrayItem item = new TrayItem(Display.getCurrent().getSystemTray(),
-                SWT.NONE);
-        final Image image = Activator.getImageDescriptor("icons/css.ico")
-                .createImage();
-        item.setImage(image);
-        item.setToolTipText("RCPMail - Tray Icon");
-        // There should be exactly one workbench window when this is being called.
-        IWorkbenchWindow w = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-            w.getShell().setVisible(false);
-        item.addSelectionListener(new SelectionAdapter() {
-            public void widgetSelected(SelectionEvent e) {
-                Shell workbenchWindowShell = w.getShell();
-                workbenchWindowShell.setVisible(true);
-                workbenchWindowShell.setActive();
-                workbenchWindowShell.setFocus();
-                workbenchWindowShell.setMinimized(false);
-                image.dispose();
-                item.dispose();
-                minimizedToTray = false;
-            }
-        });
-        // Create a Menu
-        final Menu menu = new Menu(w.getShell(), SWT.POP_UP);
-        // Create the exit menu item.
-        final MenuItem exit = new MenuItem(menu, SWT.PUSH);
-        exit.setText("Exit");
-        // Create the open menu item.
-        final MenuItem open = new MenuItem(menu, SWT.PUSH);
-        open.setText("Open");
-        // make the workbench visible in the event handler for exit menu item.
-        open.addListener(SWT.Selection, new Listener() {
-            public void handleEvent(Event event) {
-                Shell workbenchWindowShell = w.getShell();
-                workbenchWindowShell.setVisible(true);
-                workbenchWindowShell.setActive();
-                workbenchWindowShell.setFocus();
-                workbenchWindowShell.setMinimized(false);
-                image.dispose();
-                item.dispose();
-                open.dispose();
-                exit.dispose();
-                menu.dispose();
-                minimizedToTray = false;
-            }
-        });
-        item.addListener(SWT.MenuDetect, new Listener() {
-            public void handleEvent(Event event) {
-                menu.setVisible(true);
-            }
-        });
-
-        // Do a workbench close in the event handler for exit menu item.
-        exit.addListener(SWT.Selection, new Listener() {
-            public void handleEvent(Event event) {
-                image.dispose();
-                item.dispose();
-                open.dispose();
-                exit.dispose();
-                menu.dispose();
-                PlatformUI.getWorkbench().close();
-            }
-        });
+    @Override
+    public void postStartup() {
+        IEclipseContext context = PlatformUI.getWorkbench().getService(IEclipseContext.class);
+        // Initialise a perspective saver.
+        TrayListener trayListener = ContextInjectionFactory.make(TrayListener.class, context);
+        trayListener.setTrayIcon(trayIcon);
     }
 
     @Override
     public boolean preShutdown() {
-        if (minimizedToTray) {
+        if (trayIcon.isMinimized()) {
         	return super.preShutdown();
         } else {
-            minimizeToTray();
-            minimizedToTray = true;
+            trayIcon.minimize();
             return false;
         }
     }
---
+++ plugins/org.csstudio.product.trayicon/src/org/csstudio/trayicon/TrayIcon.java
@@ -0,0 +1,97 @@
+package org.csstudio.trayicon;
+
+import org.csstudio.utility.product.Activator;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.TrayItem;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+
+public class TrayIcon {
+	
+	public static String TOOLTIP = "CS-Studio";
+	public static String IMAGE = "icons/css.ico";
+    private TrayItem item;
+    final private Image image = Activator.getImageDescriptor(IMAGE).createImage();
+    private Menu menu; 
+    private MenuItem open;
+    private MenuItem exit;
+    IWorkbenchWindow w;
+
+	private boolean minimized;
+	
+	private void dispose() {
+        item.dispose();
+        open.dispose();
+        exit.dispose();
+        menu.dispose();
+	}
+	
+	public void raiseWindow(Shell shell) {
+        shell.setVisible(true);
+        shell.setActive();
+        shell.setFocus();
+        shell.setMinimized(false);
+	}
+	
+	public void minimize() {
+		item = new TrayItem(Display.getCurrent().getSystemTray(), SWT.NONE);
+        item.setImage(image);
+        item.setToolTipText(TOOLTIP);
+        // There should be exactly one workbench window when this is being called.
+        w = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+            w.getShell().setVisible(false);
+        item.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                raiseWindow(w.getShell());
+                item.dispose();
+                minimized = false;
+            }
+        });
+        // Create a Menu
+        menu = new Menu(w.getShell(), SWT.POP_UP);
+        // Create the exit menu item.
+        exit = new MenuItem(menu, SWT.PUSH);
+        exit.setText("Exit");
+        // Create the open menu item.
+        open = new MenuItem(menu, SWT.PUSH);
+        open.setText("Open");
+        open.addListener(SWT.Selection, new Listener() {
+            public void handleEvent(Event event) {
+            	unminimize();
+            }
+        });
+        item.addListener(SWT.MenuDetect, new Listener() {
+            public void handleEvent(Event event) {
+                menu.setVisible(true);
+            }
+        });
+
+        exit.addListener(SWT.Selection, new Listener() {
+            public void handleEvent(Event event) {
+            	dispose();
+                PlatformUI.getWorkbench().close();
+            }
+        });
+        minimized = true;
+	}
+
+	public boolean isMinimized() {
+		return minimized;
+	}
+
+	public void unminimize() {
+        raiseWindow(w.getShell());
+        dispose();
+        minimized = false;
+	}
+
+}
